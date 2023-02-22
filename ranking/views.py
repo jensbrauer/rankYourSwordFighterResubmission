b26685a5152a6ca8fcf81bfcd1a6658f45d3624c
jensbrauer <henningsson.jens@gmail.com>
2023-02-22 14:47:20 +0000

    Add first swordfighter list view

--- ranking/views.py
+++ ranking/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import swordfighter
 
-# Create your views here.
+class swordfighterList(generic.ListView):
+    model = swordfighter
+    template_name = 'index.html'
+    paginate_by = 6
