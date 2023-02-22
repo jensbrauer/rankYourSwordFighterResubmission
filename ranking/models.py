987aca4d7507021b0a053d569b9747a8b4ec8c96
jensbrauer <henningsson.jens@gmail.com>
2023-02-22 13:11:52 +0000

    add swordfighter model

--- ranking/models.py
+++ ranking/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class swordfighter(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    appears_on = models.CharField(max_length=100)
+    description = models.TextField()
+    profile_img = CloudinaryField('image', default='placeholder')
+
+    def __str__(self):
+        return self.name
+    
+
+
