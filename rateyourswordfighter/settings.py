6295558fa2904fe0a981a1b6aa3430281de481b2
jensbrauer <henningsson.jens@gmail.com>
2023-02-22 11:31:02 +0000

    update ALLOWED_HOSTS

--- rateyourswordfighter/settings.py
+++ rateyourswordfighter/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['rate-your-swordfighter.heroku.com', 'localhost']
+ALLOWED_HOSTS = ['rate-your-swordfighter.herokuapp.com', 'localhost']
 
 
 # Application definition
