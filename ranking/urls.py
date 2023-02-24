f3e4248f7c56e1554d802415543a950b75e7339f
jensbrauer <henningsson.jens@gmail.com>
2023-02-24 19:02:29 +0000

    Add character page for swordfighters

--- ranking/urls.py
+++ ranking/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.swordfighterList.as_view(), name='home')
+    path('', views.swordfighterList.as_view(), name='home'),
+    path('<slug:slug>/', views.swordfighterDetail.as_view(), name='swordfighter_detail'),
 ]
--- ranking/views.py
+++ ranking/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import swordfighter
 
 class swordfighterList(generic.ListView):
     model = swordfighter
     template_name = 'index.html'
-    paginate_by = 6
+
+
+class swordfighterDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = swordfighter.objects
+        character = get_object_or_404(queryset, slug=slug)
+
+        return render(
+            request,
+            "character_page.html",
+            {
+                "character": character
+            }
+        )
