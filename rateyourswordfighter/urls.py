013625046ba97da9ab2a233a43628518892f0e5f
jensbrauer <henningsson.jens@gmail.com>
2023-02-25 09:06:27 +0000

    Add allauth authorization

--- rateyourswordfighter/urls.py
+++ rateyourswordfighter/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('ranking.urls'), name='ranking_urls')
+    path('', include('ranking.urls'), name='ranking_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
