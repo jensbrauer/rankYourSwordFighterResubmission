6f4a749a3b3d7c663b3e60a91f976ff6aad8e972
jensbrauer <henningsson.jens@gmail.com>
2023-03-01 14:19:43 +0000

    Add widgets for forms bootstrap styling

--- ranking/forms.py
+++ ranking/forms.py
@@ -5,3 +5,10 @@ class SwordfighterForm(forms.ModelForm):
     class Meta:
         model = Swordfighter
         fields = ('name', 'appears_on', 'description', 'profile_img')
+
+        widgets = {
+            'name' : forms.TextInput(attrs={'class': 'form-control'}),
+            'appears_on' : forms.TextInput(attrs={'class': 'form-control'}),
+            'description' : forms.Textarea(attrs={'class': 'form-control'}),
+            'profile_img' : forms.FileInput(attrs={'class': 'form-control'})
+        }
